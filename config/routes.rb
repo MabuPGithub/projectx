85ae7fc46fa03d066277273aab2fa0d8412e322b
MabuPGithub <mylespison@yahoo.com>
2022-03-07 16:00:32 +0800

    setup pages with fixed seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_for :admins
-  root to: "home#index"
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  get '/admins' => 'users#admins', as: 'admins'
+  get '/member' => 'users#member', as: 'member'
+  get '/command_center/super_admin' => 'users#super_admin', as: 'super_admin'
+#  root to: "users#super_admin"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
