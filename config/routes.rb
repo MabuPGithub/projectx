b5c2fd7cb9b549a14952f966ac3de49fb50e0c49
MabuPGithub <mylespison@yahoo.com>
2022-03-07 17:28:44 +0800

    added logout and fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   get '/member' => 'users#member', as: 'member'
   get '/command_center/super_admin' => 'users#super_admin', as: 'super_admin'
 #  root to: "users#super_admin"
-  
   resources :users
+
+  root :to => redirect("/users/sign_in")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
